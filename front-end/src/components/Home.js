7a01a1e022fdd94769b1c06833104cf71c25baaa
Pasan <pasan96tennakoon@gmail.com>
2020-12-27 08:38:09 -0600

    test

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,46 +1,26 @@
 import React from 'react';
 import { StyleSheet, View, TouchableOpacity } from 'react-native';
 
-import { Image } from 'react-native';
 
 export default class Home extends React.Component {
 
   state = {
-    student_id: '',
-    icon_list : ["media.png", "notification.png", "papers.jpeg", "profile.png"],
+    student_id: ''
   }
 
-  continue(icon){
-
-    const {student_id, icon_list} = this.state;
-    if(icon == icon_list[0])
-    {
-      this.props.navigation.navigate('Media',{ student_id },)
-    }
-  }
 
   componentDidMount(){
 
     this.setState({ student_id: this.props.navigation.state.params.student_id}, function() {
       console.log("student id updated");
     });
+
    }
 
    render() {
       
-    const { icon_list } = this.state; 
-
       return (
-          <View style = {styles.view}>
-
-            {icon_list.map((item, i) => 
-            {
-              return (
-                <TouchableOpacity onPress={() => this.continue(item)}style = {styles.thumbnail} key={i} >
-                  <Image style = {styles.thumbnail} source={require('../images/'+item)} />
-                </TouchableOpacity>
-              );
-            })}
+          <View>
           
           </View>
         
@@ -50,19 +30,4 @@ export default class Home extends React.Component {
 
 var styles = StyleSheet.create({
 
-  view: {
-    //alignContent: 'flex-start',
-    alignItems: 'center',
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    height: '100%',
-    justifyContent: 'center'
-   //position: "absolute"
-  },
-  thumbnail: {
-    margin: 10,
-    width: 120,
-    height: 120,
-  },
  });
